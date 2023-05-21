05e243bed8b50dff52d75b77f26e58874219d509
rajesh-sv <125382031+rajesh-sv@users.noreply.github.com>
2023-05-21 18:52:43 +0530

    Complete exercises 01 to 07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeItems) {
+  const remove = new Set([...removeItems]);
+  return array.filter((item) => !remove.has(item));
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+  if(!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0) {
+    return "ERROR";
+  }
+  if(end < start) {
+    const temp = start;
+    start = end;
+    end = temp;
+  }
+  return ((end*(end+1))>>1) - ((start*(start-1))>>1);
 };
 
 // Do not edit below this line
